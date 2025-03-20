e143290fcce304e351e7c4d3aa36f4e85a650ca6
Your Name <you@example.com>
2025-03-20 15:26:38 +0800

    Fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+require('dotenv').config();
 
 // Create an instance of Express app
 const app = express();
